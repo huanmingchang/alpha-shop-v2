18a35a62c4135228ed33fd66d794529d9112ed28
huanmingchang <huanming1988@gmail.com>
2022-01-04 07:26:22 +0800

    feat: add footer html and styles

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ const imgCollections = {
   cartIcon: './../src/assets/shoppingcart-icon-daymode.svg',
   switchIconMoon: './../src/assets/moon-icon-daymode.svg',
   switchIconSun: './../src/assets/sun-icon-nightmode.svg',
+  fbIcon: './../src/assets/fb-icon-daymode.svg',
+  igIcon: './../src/assets/instagram-icon-daymode.svg',
+  whatsappIcon: './../src/assets/whatsapp-icon-daymode.svg',
 }
 
 const navCategoriesContent = [
@@ -18,12 +21,18 @@ const navCategoriesContent = [
 ]
 
 const navCategory = document.querySelector('.nav__wrapper__category')
+const hamburger = document.querySelector('.nav__hamburger')
 
 document.querySelector('.nav__logo--img').src = imgCollections.logo
 document.querySelector('.nav__hamburger--img').src = imgCollections.hamburger
 document.querySelector('.search').src = imgCollections.searchIcon
 document.querySelector('.cart').src = imgCollections.cartIcon
 document.querySelector('.switch').src = imgCollections.switchIconMoon
+document.querySelector('.footer__container__logo--img').src =
+  imgCollections.logo
+document.querySelector('.fb').src = imgCollections.fbIcon
+document.querySelector('.ig').src = imgCollections.igIcon
+document.querySelector('.whatsapp').src = imgCollections.whatsappIcon
 
 function renderNavCategory() {
   let htmlContent = ''
@@ -33,8 +42,14 @@ function renderNavCategory() {
             <span class="nav__wrapper__category--content">${navCategoriesContent[i]}</span>
           </a>
     `
-    navCategory.innerHTML = htmlContent
   }
+  navCategory.innerHTML = htmlContent
+}
+
+function hamburgerOnClick(event) {
+  console.log('click')
 }
 
 renderNavCategory()
+
+hamburger.addEventListener('click', hamburgerOnClick)
